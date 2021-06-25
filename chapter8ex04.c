b9dbcb4c1cfb0872d1e5aea4f67d0574111040a8
AngeL Lugo <83649404+1909107@users.noreply.github.com>
2021-06-24 19:41:12 -0500

    Create chapter8ex04.c

---
+++ chapter8ex04.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+char line[100]; 
+int main() {
+    int n, i;
+    printf("Ingrese un numero ");
+fgets(line, sizeof(line), stdin);
+sscanf(line, "%d", &n);
+    for (i = 1; i <= 10; ++i) {
+        printf("%d * %d = %d \n", n, i, n * i);
+    }
+    return 0;
+}
